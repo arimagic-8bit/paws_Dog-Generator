f763eba9a5eff96ffb94e3770b98a1d375ef758b
arimagic-8bit <baggerman1994@gmail.com>
2020-07-09 21:23:52 +0200

    Add route to API to get random dog photo and works in local. Test new route to get some dog breed images with searchbar

--- javascript/dogApi.js
+++ javascript/dogApi.js
@@ -1,3 +1,59 @@
-const connectToAPI () {
-    const 
+
+const randomButton = document.querySelector('.random-button');
+
+function getRandomPhoto() {
+
+    // save the place where we want to put the img on HTML
+    const div = document.querySelector(".dog-img");
+
+    if(div.hasChildNodes()) {
+        div.removeChild(div.childNodes[0]);
+    }
+
+    fetch('https://dog.ceo/api/breeds/image/random')
+    .then((response) => {
+        // save the response in an legible object
+        return response.json(); // asincro. response
+    })
+    .then((data) =>{
+        const image = document.createElement('img');
+        image.src=`${data.message}`;
+        image.alt="Random Dog Image";
+        image.classList.add('photo');
+        div.appendChild(image)
+    })
+    .catch((err) => {console.log('Request error')})
 }
+
+function getPhotosByBreed() {
+
+    const searchValue = document.querySelector('#search').value;
+    const selectValue = document.querySelector('#select').value;
+
+    const div = document.querySelector(".dog-img");
+
+    if(div.hasChildNodes()) {
+        div.removeChild(div.childNodes);
+    }
+
+    fetch(`https://dog.ceo/api/breed/${searchValue}/images/random/${selectValue}`)
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        data.message.forEach(photo => {
+          const div = document.createElement('div');
+            div.innerHTML = `
+            <div>
+                <img src="${photo}" alt="Dog photo" />
+            </div>
+        `;  
+        });
+        
+    })
+}
+
+
+
+
+randomButton.onclick = getRandomPhoto;
